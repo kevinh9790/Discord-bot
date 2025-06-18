466e7c302ed92357d8f36b33f9136d28258aa9b5
kevinh9790 <kevinh9790@gmail.com>
2025-06-18 21:06:35 +0800

    0618DevTest2

---
+++ commands/ForTest.js
@@ -0,0 +1,15 @@
+﻿// commands/ForTest.js
+module.exports = {
+    name: "大便",
+    description: "大便香腸",
+    execute(message) {
+      message.reply({
+        content:[
+            `想要吃嗎?`
+        ].join('\n'),
+        allowedMentions: { parse: [] },
+        embeds:[]//禁用嵌入卡片
+      });
+    },
+  };
+  
