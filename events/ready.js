4809595df90ebe8bf367adf3db0b9cf1ec3f7b89
kevinh9790 <kevinh9790@gmail.com>
2025-10-22 22:23:59 +0800

    拿掉邀請功能

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,10 @@
     name: 'ready',
     once: true,
     // === Bot 啟動後事件 ===
-    async execute(client) {
+    execute(client) {
       console.log(`✅ 已登入為 ${client.user.tag}`);
         console.log(`🛌 醒來於 ${new Date().toLocaleTimeString()}`);
 
-        client.inviteUses = new Map();
-
-        for (const [guildId, guild] of client.guilds.cache) {
-            const invites = await guild.invites.fetch();
-            client.inviteUses.set(guildId, new Map(invites.map(inv => [inv.code, inv.uses])));
-        }
-
-        console.log(`🤖 已啟動並記錄所有伺服器邀請次數`);
+        
     }
   };
